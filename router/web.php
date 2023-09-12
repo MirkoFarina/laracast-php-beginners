c52e793b5a0785c6088b61b6f1fe1676d6bf59d7
Mirko Farina <mirko.farina@tecnoimp.it>
2023-09-12 13:25:08 +0200

    clean code

--- router/web.php
+++ router/web.php
@@ -1,13 +1,4 @@
 <?php
-// $routes = [
-//     '/' => 'App/controllers/index.php',
-//     '/about' => 'App/controllers/about.php',
-//     '/contact' => 'App/controllers/contact.php',
-//     '/notes' => 'App/controllers/notes/index.php',
-//     '/note/create' => 'App/controllers/notes/create.php',
-//     '/note' => 'App/controllers/notes/show.php'
-// ];
-
 $router->get('/','App/controllers/index.php');
 $router->get('/about','App/controllers/about.php');
 $router->get('/contact','App/controllers/contact.php');
