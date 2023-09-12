c91d57c6c095cbca4412772ff902e6dcba28eaa9
Mirko Farina <mirko.farina@tecnoimp.it>
2023-09-12 13:24:57 +0200

    add comment in web file

--- router/web.php
+++ router/web.php
@@ -12,12 +12,13 @@ $router->get('/','App/controllers/index.php');
 $router->get('/about','App/controllers/about.php');
 $router->get('/contact','App/controllers/contact.php');
 
-
+// index,show, delete
 $router->get('/notes','App/controllers/notes/index.php');
 $router->get('/note','App/controllers/notes/show.php');
 $router->delete('/note','App/controllers/notes/destroy.php');
+// create
 $router->get('/note/create','App/controllers/notes/create.php');
-$router->get('/note/edit','App/controllers/notes/edit.php');
 $router->post('/notes','App/controllers/notes/store.php');
+// edit
+$router->get('/note/edit','App/controllers/notes/edit.php');
 $router->patch('/note','App/controllers/notes/update.php');
-$router->delete('/note','App/controllers/notes/destroy.php');
