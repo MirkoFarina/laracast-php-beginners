a866bd6dfb2b5b1ee33db1b582e652e35b650844
Mirko Farina <mirko.farina@tecnoimp.it>
2023-10-02 14:49:04 +0200

    autoload clas whit composer

--- public/index.php
+++ public/index.php
@@ -3,19 +3,14 @@
 use Core\FormException;
 use Core\Router;
 use Core\Session;
-
-session_start();
-
 const BASE_PATH = __DIR__ . '/../';
+
+
+require BASE_PATH . 'vendor/autoload.php';
+session_start();
 
 require BASE_PATH . 'Core/function.php';
 
-
-spl_autoload_register(function ($class) {
-    $class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
-    require base_path($class . '.php');
-});
- 
 require base_path('App/bootstrap.php');
 
 $router = new Router();
